c0255314c8e3324db067fec7c22be11273f7bdb7
Capricieux617-tomo <capricieux.6.17@gmail.com>
2024-07-31 01:03:57 +0900

    kadai_005を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,41 @@
+console.log('体重(kg)');
+//体重の宣言
+let userWeight;
+
+//値の代入
+userWeight = '68';
+
+//コンソールへの出力
+console.log(userWeight);
+
+console.log('身長(m)');
+//身長の宣言
+let userHeight;
+
+//値の代入
+userHeight = '1.7';
+
+//コンソールへの出力
+console.log(userHeight);
+
+console.log('BMI指数')
+console.log('[体重(kg)]÷[身長(m)×身長(m)]');
+
+console.log('計算結果')
+//BMI計算式の宣言
+let bmi;
+
+//値の代入
+bmi = (userWeight) / (userHeight * userHeight);
+
+//コンソールへの出力
+console.log(bmi);
+
+
+
+
+
+
+
+
+
